8fdb881d9330e759bd25280f73be2d96e8a5fc48
Kavilakshan12 <kavilakshanvijayakumar@gmail.com>
2025-04-27 14:44:48 +0530

    fixed the error

--- Frontend/src/Components/Modals/CreatePostModal.jsx
+++ Frontend/src/Components/Modals/CreatePostModal.jsx
@@ -135,7 +135,7 @@ const CreatePostModal = () => {
     position: "absolute",
     zIndex: 0,
     opacity: 0.5,
-    animation: "float 20s infinite ease-in-out",
+    animation: "float 20sec infinite ease-in-out",
   };
 
   return (
